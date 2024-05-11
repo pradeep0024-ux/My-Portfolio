9a97f9addc53f687c98e31fc3f0411e92953b81b
pradeep0024-ux <83303824+pradeep0024-ux@users.noreply.github.com>
2024-05-11 23:36:06 +0530

    First Commit

---
+++ src/utils/Themes.js
@@ -0,0 +1,24 @@
+
+export const darkTheme = {
+    bg: "#0B3954",
+    bgLight: "#0D4B6E",
+    primary: "#4ECDC4",
+    text_primary: "#F7FFF7",
+    text_secondary: "#8D99AE",
+    card: "#1A535C",
+    card_light: "#1E6F89",
+    button: "#4ECDC4",
+    white: "#FFFFFF",
+    black: "#000000",
+}
+
+
+export const lightTheme = {
+    bg:"#FFFFFF",
+    bgLight: "#f0f0f0",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
+}
